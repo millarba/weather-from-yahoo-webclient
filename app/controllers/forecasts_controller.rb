c78c954d69d66da01f2267f5bd2aa0d4505823d2
millarba <millarba@gmail.com>
2018-03-26 17:53:54 -0500

    update all gems

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,7 +1,10 @@
 class ForecastsController < ApplicationController
 
   def index
-    @forecast = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22newyork%2C%20ny%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body
+    city = params[:city] || "chicago"
+    state = params[:state] || "il"
+
+    @forecast = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22#{ city }%2C%20#{ state }%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body
 
     @temp = @forecast["query"]["results"]["channel"]["item"]["condition"]["temp"]
     @city = @forecast["query"]["results"]["channel"]["location"]["city"]
